1e3fb6926c2152f1154a50f2b48062b0acd79b1f
Thulasi Aravindh <tl392@njit.edu>
2026-04-01 02:40:54 -0400

    Url update

--- main.py
+++ main.py
@@ -115,4 +115,4 @@ async def divide_route(operation: OperationRequest):
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
